aca4b87c5d05926585c92518112bdc8b6272a67d
William Wong <compulim@users.noreply.github.com>
2023-10-11 08:12:07 +0000

    Add sample activity

--- src/app/data/sampleActivity.ts
+++ src/app/data/sampleActivity.ts
@@ -2,12 +2,28 @@ export default {
   conversation: { id: 'c-00001' },
   from: { id: 'bot' },
   type: 'message',
-  text: `Sure, you should override the default proxy settings[1]\u200B[2], when your proxy server requires authentication[3]. Here's a fourth citation[4].
-
-[1]: https://support.microsoft.com/en-us/windows/use-a-proxy-server-in-windows-03096c53-0554-4ffe-b6ab-8b1deee8dae1 "Use a proxy server in Windows"
-[2]: https://learn.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-proxy-server-settings "Configure proxy server settings - Windows Server"
-[3]: cite:1 "Introduction Configuring proxy settings is a fundamental aspect..."
-[4]: cite:2 "This citation has no name"
-`,
-  entities: []
+  attachments: [
+    {
+      type: 'https://schema.org/ReviewAction',
+      content: {
+        '@context': 'https://schema.org',
+        '@type': 'ReviewAction',
+        review: {
+          '@type': 'Review',
+          reviewAspect: 'Great! Please rate your experience.'
+        },
+        target: {
+          '@type': 'EntryPoint',
+          actionPlatform: 'https://directline.botframework.com',
+          contentType: 'application/vnd.microsoft.directline.activity;type=postback'
+        },
+        result: {
+          '@type': 'Review',
+          reviewRating: {
+            'ratingValue-input': 'required'
+          }
+        }
+      }
+    }
+  ]
 };
